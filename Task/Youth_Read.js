754b838ade259e52af2a6ac62511a7faad9c20a4
Best <Sunert@users.noreply.github.com>
2020-09-24 22:34:00 +0800

    fix error

--- Task/Youth_Read.js
+++ Task/Youth_Read.js
@@ -1,5 +1,5 @@
 /*
-更新时间: 2020-09-24 20:15
+更新时间: 2020-09-24 22:15
 赞赏:中青邀请码`46308484`,农妇山泉 -> 有点咸，万分感谢
 
 请自行抓包，阅读文章和看视频，倒计时转一圈显示青豆到账即可，多看几篇文章和视频，获得更多包数据，抓包地址为"https://ios.baertt.com/v5/article/complete.json"，在Github Actions中的Secrets新建name为'YOUTH_READ'的一个常量，拷贝抓包的请求体到下面Value的文本框中，值越多，获得青豆次数阅读，本脚本不包含任何推送通知
@@ -8,7 +8,7 @@
 
 */
 
-let s = 300000 //等待延迟30s
+let s = 30000 //等待延迟30s
 const $ = new Env("中青看点")
 //const notify = $.isNode() ? require('./sendNotify') : '';
 let ReadArr = [], articlebody ='';
