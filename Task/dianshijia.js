95022ca1532bd02f0f8e6789b8f37bb7a9b810ba
Best <47924389+Sunert@users.noreply.github.com>
2020-11-16 11:09:44 +0800

    Update dianshijia.js

--- Task/dianshijia.js
+++ Task/dianshijia.js
@@ -98,12 +98,12 @@ if (isGetCookie = typeof $request !== 'undefined') {
     console.log(`------------- 共${tokenArr.length}个账号`)
     if(new Date().getTimezoneOffset()/60 != '-8'&&$.time('HH')<'16'){
         time = new Date(new Date(new Date().toLocaleDateString()).getTime())/1000
+       console.log(time)
        } else if(new Date().getTimezoneOffset()/60 != '-8'&&$.time('HH')>'16'){
         time = new Date(new Date(new Date().toLocaleDateString()).getTime())/1000+24*60*60
        } else {
         time = new Date(new Date(new Date().toLocaleDateString()).getTime())/1000
     }
-   console.log(time)
   for (let i = 0; i < tokenArr.length; i++) {
     if (tokenArr[i]) {
       signheaderVal = tokenArr[i];
