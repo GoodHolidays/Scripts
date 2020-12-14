d4a46fce6a45d6efac0a7e9e759d0ffdb2abc98b
Best <47924389+Sunert@users.noreply.github.com>
2020-12-14 21:32:58 +0800

    Update moji.js

--- Task/moji.js
+++ Task/moji.js
@@ -1,6 +1,6 @@
 /*
 支持boxjs手动修改位置，可直接输入中文地区名
-更新时间 2020-12-10 20:13
+更新时间 2020-12-14 21:13
 */
 const $ = new Env('墨迹天气')
 const City = encodeURIComponent($.getdata('city')||"北京")
@@ -13,12 +13,11 @@ let daylys = $.getdata('day_desc') || true, //每日天气
 !(async() => {
   await SearchCity();
   await Weather();
-  await Indexs();
+  await TodayReport();
   await showmsg()
  })()
     .catch((e) => $.logErr(e))
     .finally(() => $.done())
-
 
 function Weather() {
   return new Promise((resolve, reject) =>{
@@ -56,8 +55,6 @@ try {
   })
 }
 
-
-
 function SearchCity() {
   return new Promise((resolve) =>{
    let cityurl =  {
@@ -85,6 +82,7 @@ function SearchCity() {
     })
   })
 }
+
 function mapSkycon(skycon) {
   const map = {
     "晴": [
@@ -155,58 +153,62 @@ function mapSkycon(skycon) {
   return map[skycon];
 }
 
+function windSpeed(speed) {
+  const map = {
+     0: "无风",
+     1: "软风",
+     2: "轻风",
+     3: "微风",
+     4: "和风",
+     5: "劲风",
+     6: "强风",
+     7: "疾风",
+     8: "大风",
+     9: "烈风",
+     10: "狂风",
+     11: "暴风",
+     12: "飓风"
+   }
+  const wind_desc = $.weather.data.wind_desc.value
+  return `${map[wind_desc[0]]}`
+}
 
-function showmsg() {
-       let icon = "❓"
-       console.log("\n您的地区为〈"+$.weather.data.city+"〉")
-       nowweather = $.weather.data.weather_desc //当前天气
-       nowtemp = $.weather.data.temp.value+$.weather.data.temp.unit  //当前温度
-       nowwindval = $.weather.data.wind_desc.winddir+$.weather.data.wind_desc.value+$.weather.data.wind_desc.unit  //当前风速
-       nowhum = $.weather.data.humidity  //当前湿度
-       aqidesc = $.weather.data.aqi_desc //空气质量
-       sunrise = $.weather.data.sunset.sunrise
-       sundown = $.weather.data.sunset.sundown
-       daytemp = $.weather.data.forecast_day[0].temp_low.value+"℃"+"-"+$.weather.data.forecast_day[0].temp_high.value+"℃"
-       $.desc = "   当天温度: "+daytemp+"   实时温度🌡:"+nowtemp+"\n  " +` 天气: ${mapSkycon(nowweather)[0]}`+"   风速🌪:"+ nowwindval + "\n   空气质量🌬:"+aqidesc+"    湿度☔️:"+nowhum+"\n"
-        if(daylys== 'true'){
-       $.desc += "【每周天气】\n"
-      }
-        for ( Dayweather of $.weather.data.forecast_day){
-        week = Dayweather.predict_week        // 日期     
-        date = Dayweather.predict_date
-       
-        Daysweather = Dayweather.weather_desc_day     //当日天气
-        Winds = Dayweather.wind_desc_day.winddir+Dayweather.wind_desc_day.value+ Dayweather.wind_desc_day.unit   //当日风速
-        temps = Dayweather.temp_low.value+Dayweather.temp_low.unit+"至"+Dayweather.temp_high.value+Dayweather.temp_high.unit                      //当日温度
-        if(reduction== 'true') {$.log(" "+ date+" "+Daysweather+" "+ temps+" "+Winds)
-        }
-       if(daylys== 'true'){
-        $.desc +=   "   "+week+" "+mapSkycon(Daysweather)[0]+" "+ temps+" "+Winds+"\n"
-        }
-       };
-     if(hourlys == 'true'){
-      $.desc +=  "【未来6小时变化预报】\n"
-    }
-     for ( i=0;i <6 ;i++){
-     Hourlyweather = $.weather.data.hourly
+function HourlyReport() {
+  const Hourlyweather = $.weather.data.hourly
+   for ( i=0;i < 6 ;i++){
         hours = Hourlyweather[i].temperature.hour
         hourweather = Hourlyweather[i].temperature.condition
-        hourWinds = Hourlyweather[i].wind.wind_desc.winddir+Hourlyweather[i].wind.wind_desc.value+ Hourlyweather[i].wind.wind_desc.unit
+        hourly_Skycon = mapSkycon(hourweather) ? mapSkycon(hourweather)[0]:"   "+hourweather
+        hourWinds = Hourlyweather[i].wind.wind_desc.winddir+Hourlyweather[i].wind.wind_desc.value+Hourlyweather[i].wind.wind_desc.unit
         hourtemps = Hourlyweather[i].temperature.temp.value+Hourlyweather[i].temperature.temp.unit
      if(hourlys == 'true'&& i==0 ){
-        $.desc += "   "+hours+":00  "+mapSkycon(Hourlyweather[i].temperature.condition)[0]+"  "+ hourtemps+"  "+hourWinds +"\n"
+        $.desc += "   "+hours+":00  "+hourly_Skycon+"  "+ hourtemps+"  "+hourWinds +"\n"
       } else if(hourlys == 'true'&& (hourweather != Hourlyweather[i-1].temperature.condition || hourtemps != Hourlyweather[i-1].temperature.temp.value+"℃")){
-        $.desc += "   "+hours+":00  "+mapSkycon(Hourlyweather[i].temperature.condition)[0]+"  "+ hourtemps+"  "+hourWinds +"\n"
+        $.desc += "   "+hours+":00  "+hourly_Skycon+"  "+ hourtemps+"  "+hourWinds +"\n"
       }
      if(reduction == 'true') { 
         $.log("    "+hours+":00  "+hourweather+"  "+ hourtemps+"  "+hourWinds)
       }
+   }
+}
+
+function WeekReport() {
+  for (Dayweather of $.weather.data.forecast_day){
+        week = Dayweather.predict_week        // 日期     
+        date = Dayweather.predict_date
+       
+        Daysweather = Dayweather.weather_desc_day     //当日天气
+        Days_Skycon = mapSkycon(Daysweather) ? mapSkycon(Daysweather)[0]:"   "+Daysweather
+        Winds = Dayweather.wind_desc_day.winddir+Dayweather.wind_desc_day.value+ Dayweather.wind_desc_day.unit   //当日风速
+        temps = Dayweather.temp_low.value+Dayweather.temp_low.unit+"至"+Dayweather.temp_high.value+Dayweather.temp_high.unit                      //当日温度
+        if(reduction== 'true') {$.log(" "+ date+" "+Daysweather+" "+ temps+" "+Winds)
+        }
+        $.desc +=   "   "+week+" "+Days_Skycon+" "+ temps+" "+Winds+"\n"
      }
-   if ($.index.code == 0){
-    if(indexs== 'true'){
-       $.desc +=  "【生活指数】\n"
-      }
-       for (indexdata of $.index.indexs){
+}
+
+function IndexReport() {
+    for (indexdata of $.index.indexs){
          indexType = indexdata.indexType
          indexLevel = indexdata.indexLevel
          indexLevelDesc =indexdata.indexLevelDesc 
@@ -216,11 +218,41 @@ function showmsg() {
          }
          if(reduction == 'true'){ console.log("\n "+indexType+"  "+indexLevel+"级  "+ indexLevelDesc+"\n"+indexDesc )
           }
-         }
-       }  
+     }
+ }  
+
+function TodayReport() {
+       console.log("您的地区为〈"+$.weather.data.city+"〉")
+       nowweather = $.weather.data.weather_desc //当前天气
+       today_Skycon = mapSkycon(nowweather)? mapSkycon(nowweather)[0]:"   "+nowweather
+       nowtemp = $.weather.data.temp.value+$.weather.data.temp.unit  //当前温度
+       windDirection = $.weather.data.wind_desc.winddir
+       nowwindval = $.weather.data.wind_desc.value  //当前风速
+       nowhum = $.weather.data.humidity  //当前湿度
+       aqidesc = $.weather.data.aqi_desc //空气质量
+       sunrise = $.weather.data.sunset.sunrise
+       sundown = $.weather.data.sunset.sundown
+       daytemp = $.weather.data.forecast_day[0].temp_low.value+"℃"+"-"+$.weather.data.forecast_day[0].temp_high.value+"℃"
+       $.desc = "   当天温度: "+daytemp+"   实时温度🌡:"+nowtemp+"\n  " +` 实时天气: ${today_Skycon}`+"   风速🌪: "+ windDirection + nowwindval +"级" + windSpeed(nowwindval)+ "\n   空气质量🌬: "+aqidesc+"    湿度☔️: "+nowhum+"\n"
+}
+
+async function showmsg() {
+      if(daylys== 'true'){
+       $.desc += "【每周天气】\n"
+        await WeekReport()
+      }
+      if(hourlys == 'true'){
+        $.desc +=  "【未来6小时变化预报】\n"
+        await HourlyReport()
+      }
+      if (indexs== 'true'){
+        $.desc +=  "【生活指数】\n"
+        await Indexs();
+        await IndexReport()
+      }
          $.sub = "【今日天气】"  +`${mapSkycon(nowweather)[0]}`
          $.msg($.weather.data.city +"天气预报 "+$.weather.data.forecast_day[0].predict_date +$.weather.data.forecast_day[0].predict_week,$.sub, $.desc,{"media-url": `${mapSkycon(nowweather)[1]}`
-    })
+   })
 }
 
 function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?(this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)})):this.isQuanX()?(this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t))):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)}))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)})}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r)));let h=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];h.push(e),s&&h.push(s),i&&h.push(i),console.log(h.join("\n")),this.logs=this.logs.concat(h)}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
