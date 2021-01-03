c5bcff1c2f84d3d22461634f2e9660b7d426bd89
Best <47924389+Sunert@users.noreply.github.com>
2021-01-03 12:15:56 +0800

    Update jukan.js
    暂时取消自动提现申请

--- Task/jukan.js
+++ Task/jukan.js
@@ -85,7 +85,7 @@ if (typeof $request !== 'undefined') {
       await Stimulate(x)
      }
   if (curcash >= drawcash && wxname){
-      await realname();
+     // await realname();
       //await Withdraw() //实名未通过，强制提现，可取消此注释，不保证成功
    }
    if (signtimes&&signtimes<5){
