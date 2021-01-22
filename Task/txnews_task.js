baf73ad3fa484c82a5979092fd97e51f31d5696a
Best <47924389+Sunert@users.noreply.github.com>
2021-01-22 21:47:15 +0800

    Update txnews_task.js

--- Task/txnews_task.js
+++ Task/txnews_task.js
@@ -60,29 +60,29 @@ let RdArr = [], VdArr = [];
       body = "event=article_read"
       $.index = i + 1;
     for(p=0;p<3;p++){
-   console.log(`-------------------------\n\n开始腾讯新闻${$.index}第${p}次阅读`)
+   console.log(`-------------------------\n\n开始腾讯新闻${$.index}第${p+1}次阅读`)
       await AutoRead();
+      console.log(`请等待10s后继续阅读${$.index}第${p+2}次任务`)
       await $.wait(10000);
-      console.log(`\n  请等待10s后继续视频${$.index}第${p+1}次任务`)
       }
     }
    console.log(`-------------------------\n\n腾讯新闻共完成阅读任务(${$.index}*3)次，阅读金币详情见App，任务全部结束`)
   }
-   if(VdArr){
+   if(VdArr!==[]){
    $.log(VdArr)
     for (var j = 0; j < VdArr.length; j++) {
     console.log("共"+VdArr.length+"次阅读任务")
       TaskUrl = VdArr[j];
       body = "event=video_read"
-      $.index = j + 1;
+      $.vdindex = j + 1;
     for(k=0;k<3;k++){
-    console.log(`-------------------------\n\n开始腾讯新闻${$.index}第${k}次看视频`)
+    console.log(`-------------------------\n\n开始腾讯新闻${$.vdindex}第${k+1}次看视频`)
       await AutoRead();
+      console.log(`请等待10s后继续视频${$.vdindex}第${k+2}次任务`)
       await $.wait(10000);
-      console.log(`\n  请等待10s后继续视频${$.index}第${k+1}次任务`)
       }
     }
-   console.log(`-------------------------\n\n共完成视频任务(${$.index}*3)次，视频金币详情见App，任务全部结束`)
+   console.log(`-------------------------\n\n共完成视频任务(${$.vdindex}*3)次，视频金币详情见App，任务全部结束`)
 }
 })()
   .catch((e) => $.logErr(e))
